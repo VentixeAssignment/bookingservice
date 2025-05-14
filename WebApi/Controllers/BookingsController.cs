5f3ca8afe056b134a96f52b686db7990bf301009
Åsa Johansson <asa.j.1989@gmail.com>
2025-05-14 13:40:19 +0200

    Add project files.

---
+++ WebApi/Controllers/BookingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookingsController(IBookingService bookingsService) : ControllerBase
+{
+    private readonly IBookingService _bookingsService = bookingsService;
+}
